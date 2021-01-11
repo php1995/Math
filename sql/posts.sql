aeb7b9e505045340c815d4aa2ddb8aa7a120ab79
Victor <github.php1995@gmail.com>
2021-01-11 16:46:26 +0300

    Delete posts.sql

--- sql/posts.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `posts`
-(
-    `id`         INT(11) AUTO_INCREMENT,
-    `title`      VARCHAR(255) NOT NULL,
-    `post_link`  VARCHAR(255) NOT NULL,
-    `datetime`   DATETIME     NOT NULL,
-    `image_link` VARCHAR(255) NOT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE = INNODB
